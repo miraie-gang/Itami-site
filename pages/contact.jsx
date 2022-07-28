69562faaa2cc3e10cb9c8d1797a01c7679fd80ba
1Itami <110174875+1Itami@users.noreply.github.com>
2022-07-28 16:20:54 +0300

    Update contact.jsx

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -14,28 +14,28 @@ export default function Home() {
         <p className="text-xl text-white/50 font-normal text-center mb-5">Other addresses where you can contact me.</p>
         <div className=" flex justify-center flex-col items-center">
             <div className="max-w-lg w-full">
-                <a target="_blank" rel="norefferer" href=" https://instagram.com/umutxyp/">
+                <a target="_blank" rel="norefferer" href="https://www.instagram.com/hz.legion/">
                     <div className="flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fab fa-instagram fa-2x" />
-                        <p className="font-semibold text-xl">@umutxyp</p>
+                        <p className="font-semibold text-xl">@hz.legion/p>
                     </div>
                 </a>
-                <a target="_blank" rel="norefferer" href="https://facebook.com/umutxyp/">
+                <a target="_blank" rel="norefferer" href="https://www.facebook.com/DwayneJohnson">
                     <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fab fa-facebook fa-2x" />
-                        <p className="font-semibold text-xl">@umutxyp</p>
+                        <p className="font-semibold text-xl">buben</p>
                     </div>
                 </a>
-                <a target="_blank" rel="norefferer" href="https://discord.com/users/615029465726320654">
+                <a target="_blank" rel="norefferer" href="https://discord.com/users/968536444530479194">
                     <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fab fa-discord fa-2x" />
-                        <p className="font-semibold text-xl">Umut#6070</p>
+                        <p className="font-semibold text-xl">Itami#1111</p>
                     </div>
                 </a>
-                <a href="mailto:umutbayraktar55@gmail.com">
+                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
                     <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fa fa-envelope fa-2x" />
-                        <p className="font-semibold text-xl">umutbayraktar55@hotmail.com</p>
+                        <p className="font-semibold text-xl">Gizli sakın acma</p>
                     </div>
                 </a>
             </div>
